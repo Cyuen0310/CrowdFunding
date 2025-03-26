31076681b25a2994ed87227600763426d1360d8e
Cyuen0310 <chanszeyuen0310@icloud.com>
2025-03-26 17:31:03 +0800

    Implement CrowdFunding contract with constructor and state variables for title, description, target, deadline, amount collected, and initiator.

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,34 @@
 pragma solidity ^0.8.9;
 
 contract crowdFunding {
-    constructor() {}
+    string public title;
+    string public description;
+    uint256 public target;
+    uint256 public deadline;
+    uint256 public amountCollected;
+    address public initiator;
+    
+    constructor(
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _duration
+        
+    ) {
+        title = _title;
+        description = _description;
+        target = _target;
+        deadline = block.timestamp + _duration;
+        amountCollected = 0;
+        initiator = msg.sender;
+    }
+
+    function donate() public payable{}
+
+    function getCurrentAmount() public view returns (uint256){}
+
+
+
+
+
 }
