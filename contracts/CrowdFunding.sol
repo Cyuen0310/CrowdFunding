f5785b5f84ea04c9f3776ea12583c98d0685c4a6
Cyuen0310 <chanszeyuen0310@icloud.com>
2025-03-29 22:09:43 +0800

    Refactor CrowdFunding contract to include campaign struct, createCampaign and backCampaign functions, and update state management for campaigns.

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -1,33 +1,88 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract crowdFunding {
-    string public title;
-    string public description;
-    uint256 public target;
-    uint256 public deadline;
-    uint256 public amountCollected;
-    address public initiator;
+
+
+// TO DO:
+/*
+    - create a campaign
+    - withdraw fund (owner only)
+    - back a campaign
+    - get the total amount of campaigns
+    - get the total amount of backers
     
-    constructor(
+*/
+
+contract CrowdFunding {
+    struct Campaign{
+        address payable owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 duration;
+        uint256 deadline;
+        uint256 fundedAmount;
+        uint256 numberOfBackers;
+        bool isActive;
+        bool isCollected;
+        mapping(address => uint256) backers; // backers[address] = amount
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public TotalCampaigns = 0;
+
+    function createCampaign(
         string memory _title,
         string memory _description,
         uint256 _target,
         uint256 _duration
+    ) public returns(uint256){
+        require (_target > 0,"Campaign target must be larger than 0");
+        require (_duration > 0,"Campaign duration must at least be 1 day");
         
-    ) {
-        title = _title;
-        description = _description;
-        target = _target;
-        deadline = block.timestamp + _duration;
-        amountCollected = 0;
-        initiator = msg.sender;
+        Campaign storage campaign = campaigns[TotalCampaigns];
+        campaign.owner = payable(msg.sender);
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.duration = _duration;
+        campaign.deadline = block.timestamp + _duration * 1 days;
+        campaign.fundedAmount = 0;
+        campaign.numberOfBackers = 0;
+        campaign.isActive = true;
+        campaign.isCollected = false;
+        TotalCampaigns++;
+        return TotalCampaigns - 1;
     }
 
-    function donate() public payable{}
+    function backCampaign(uint _id) public payable{
+        uint256 fundingValue = msg.value;
+        require (fundingValue > 0,"Funding value must be larger than 0");
+        Campaign storage campaign = campaigns[_id];
+        require (campaign.isActive == true,"Campaign is not active");
+        require (campaign.deadline > block.timestamp,"Campaign has ended");
+        require (campaign.fundedAmount > 0,"");
+        campaign.backers[msg.sender] += fundingValue;
+        campaign.fundedAmount += fundingValue;
+        if (campaign.backers[msg.sender] == 0){
+            campaign.numberOfBackers++;
+        }
+        
+        
+          
 
-    function getCurrentAmount() public view returns (uint256){}
+    }
 
+    // function getBackers(){}
+
+    // function getCampaigns(){}
+ 
+
+
+
+
+    
 
 
 
